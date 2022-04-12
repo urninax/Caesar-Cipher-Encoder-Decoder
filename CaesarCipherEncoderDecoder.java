968b1c07fa0b41b9de5e8c17b3e02bd93c83c5a8
urninax <mrfreezediables@gmail.com>
2022-04-13 00:05:14 +0300

    Release

---
+++ CaesarCipherEncoderDecoder.java
@@ -0,0 +1,81 @@
+package CaesarCipher;
+
+import java.util.*;
+
+public class CaesarCipherEncoderDecoder{
+    private static final char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    private static final List<Character> alphabetList = new LinkedList<>();
+    private static final Character[] uniques = new Character[]
+            {',', '.', ' ', ':', ';', '?', '!', '(', ')', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\''};
+    private static final List<Character> uniquesList = Arrays.asList(uniques);
+    private static boolean isRepeat;
+
+    public static void main(String[] args){
+        fillAlphabetList();
+
+        while(true){
+            List<String> options = new LinkedList<>();
+            options.add("encoder");
+            options.add("decoder");
+
+            if(! isRepeat)
+                System.out.print("Choose the option: Encoder/Decoder: ");
+
+            Scanner scanner = new Scanner(System.in);
+            String option = scanner.nextLine();
+            if(! options.contains(option.toLowerCase())){
+                System.out.print("Undefined option. Type option again: ");
+                isRepeat = true;
+                continue;
+            }
+            work(option);
+        }
+    }
+
+    public static void work(String option){
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        System.out.print("Type the index: ");
+        int index = scanner.nextInt();
+
+        System.out.println("Type the text: ");
+        Scanner scanner1 = new Scanner(System.in);
+        String text = scanner1.nextLine();
+        char[] textToCharArray = text.toCharArray();
+
+        for(char c : textToCharArray){
+            if(uniquesList.contains(c)){
+                stringBuilder.append(c);
+                continue;
+            }
+
+            boolean isUpperCased = ! alphabetList.contains(c);
+
+            int futureIndex;
+            if(option.equalsIgnoreCase("encoder")){
+                futureIndex = alphabetList.indexOf(Character.toLowerCase(c))+index;
+                if(futureIndex > 25)
+                    futureIndex -= 25;
+            }else{
+                futureIndex = alphabetList.indexOf(Character.toLowerCase(c))-index;
+                if(futureIndex < 0)
+                    futureIndex += 25;
+            }
+
+            if(isUpperCased){
+                stringBuilder.append(String.valueOf(alphabetList.get(futureIndex)).toUpperCase());
+            }else{
+                stringBuilder.append(alphabetList.get(futureIndex));
+            }
+        }
+        System.out.println("Translate: "+stringBuilder+"\n");
+        isRepeat = false;
+    }
+
+    public static void fillAlphabetList(){
+        for(char c : alphabet){
+            alphabetList.add(c);
+        }
+    }
+}
